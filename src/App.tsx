0059239a10350f0938c0261452365bd4ed5f0acc
Natan Boldt <natanboldt@gmail.com>
2021-05-05 18:05:33 -0700

    Definindo estilos globais e montando layout

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import GlobalStyles from "./styles/GlobalStyles";
+
+function App() {
+  return (
+    <>
+      <h1>Olá mundo!</h1>
+      <GlobalStyles />
+    </>
+  );
+}
+
+export default App;
---
+++ src/styles/colors.ts
@@ -0,0 +1,20 @@
+const colors = {
+  primary: "#36393f",
+  secondary: "#2f3136",
+  tertiary: "rgb(32,34,37)",
+  quaternary: "#292b2f",
+  quinary: "#393d42",
+  senary: "#828386",
+  white: "#fff",
+  gray: "#8a8c90",
+  chat_input: "rgb(64,68,75)",
+  symbol: "#74777a",
+  notification: "#f84a4b",
+  discord: "#6e86d6",
+  mention_detail: "#f9a839",
+  mention_message: "#413f3f",
+  link: "#5d80d6",
+  rocketseat: "#6633cc",
+};
+
+export default colors;
